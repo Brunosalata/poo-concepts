34768708741b0152eea50445235ad33657b266fa
Bruno Salata <brunoslima@gmail.com>
2024-09-12 16:22:49 -0300

    first commit

---
+++ src/main/java/com/brunosalata/Main.java
@@ -0,0 +1,13 @@
+package com.brunosalata;
+
+/**
+ * @author Bruno Salata Lima - 16/05/2023
+ * github.com/Brunosalata
+ * @version 2.0.3
+ * @project mBioLabv3
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
